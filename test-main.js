1b2535189933fdc89ff6cf3905c5d185cbf5c02c
Yong Liu <liuy97@gmail.com>
2016-09-03 11:53:59 +0000

    add angular testing config

--- test-main.js
+++ test-main.js
@@ -28,41 +28,17 @@ System.config({
   map: {
     'rxjs': 'node_modules/rxjs',
     '@angular2-material': 'node_modules/@angular2-material',
+    '@angular/core/testing': 'node_modules/@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'node_modules/@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'node_modules/@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'node_modules/@angular/http/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'node_modules/@angular/router/bundles/router-testing.umd.js',
+    '@angular/forms/testing': 'node_modules/@angular/forms/bundles/forms-testing.umd.js',
     '@angular': 'node_modules/@angular'
   },
   packages: {
-    '@angular/common': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/compiler': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/core': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/forms': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/http': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/platform-browser': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/platform-browser-dynamic': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
-    '@angular/router': {
-      main: 'index.js',
-      defaultExtension: 'js'
-    },
     '@angular2-material/core': {
         format: 'cjs',
         defaultExtension: 'js',
