f87a09e96b950ba3cda3691231f47ff0bc937735
Yong Liu <liuy97@gmail.com>
2016-09-17 05:57:11 +0000

    add enableprodmode

--- src/client/app/main-prod.ts
+++ src/client/app/main-prod.ts
@@ -2,8 +2,11 @@
  * Bootstraps the application and makes the ROUTER_PROVIDERS and the APP_BASE_HREF available to it.
  * @see https://angular.io/docs/ts/latest/api/platform-browser-dynamic/index/bootstrap-function.html
  */
+import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './app.module.ngfactory';
 import { platformBrowser } from '@angular/platform-browser';
+
+enableProdMode();
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 
